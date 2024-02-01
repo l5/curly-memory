403e5cd4cd9e3fefe23256f069fcdac55097cda5
l5 <277705+l5@users.noreply.github.com>
2024-02-01 23:31:18 +1300

    Removed duplicate program description

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 const { program } = require("commander");
 const generateItinerary = require('./commands/generateItinerary')
 const lg = "en" // default language
-program.version("0.0.1").description("Itinerary PDF from yaml generator");
+program.version("0.0.1").description("Itinerary PDF from yaml generator. Generates PDF file from trips/<trip>/itinerary.yaml");
 
 program
     .option('-t, --trip [trip]', 'Trip')
-    .description('Generates PDF file from trips/<trip>/itinerary.yaml')
 
 program.parse(process.argv);
 var trip = program.opts().trip
