91c33fdd983429ff87efb4230d46799868ea0ac6
l5 <277705+l5@users.noreply.github.com>
2024-02-01 23:15:44 +1300

    Removed language selection

--- index.js
+++ index.js
@@ -5,19 +5,12 @@ var lg = "en" // default language
 program.version("0.0.1").description("Itinerary PDF from yaml generator");
 
 program
-    .option('-l, --language [language]', 'Language')
     .option('-t, --trip [trip]', 'Trip')
     .description('Generates PDF file from trips/<trip>/itinerary.yaml')
-// program
-//     .option('-t, --trip [trip]', 'Trip')
-//     .description('Generates PDF file from trips/<trip>/itinerary.yaml')
 
 program.parse(process.argv);
 var trip = program.opts().trip
 if (program.opts().language) {
     lg = program.opts().language
     generateItinerary(lg, trip)
-} else {
-    generateItinerary('en', trip)
-    // generatePdf('de')
 }
